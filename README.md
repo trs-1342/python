277f99a605d334017d19525cb519705c11873ca0
trs-1342 <socialmediaoftrs1342@gmail.com>
2026-05-19 11:45:54 +0300

    variable konusu eklendi

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ python hakkinda ogrendiklerimi paylastigim bir repodur.
 
 `print`: ekrana cikti verir
 
-girdiler: bir ust satira kapsamasi icin soldan n kadar bosluk birakilir, tum py dosyasinda ayni sayida girdi birakilmali.
+`girdiler`: bir ust satira kapsamasi icin soldan n kadar bosluk birakilir, tum py dosyasinda ayni sayida girdi birakilmali.
 
-yorum satirlari: # icin tek satir, """ """ baslangic ve bitisle bir cok yorum satiri
+`yorum satirlari`: # icin tek satir, """ """ baslangic ve bitisle bir cok yorum satiri
+
+`variable`(`degiskenler`): islemden gececek olan degerleri tutmamiza yarayan bir kutu
---
+++ py-codes/variable.py
@@ -0,0 +1,32 @@
+# variable
+
+h=13 # int tam sayi
+o=42.13 # float ondalikli sayi
+text="trs" # string metinsel ifade
+
+text="python" # tip degistirmesi mumkun
+
+print(h)
+print(o)
+print(text)
+
+# variable casting: bir veri turunu baska bir veri tipine donusturmeye yarar
+x=str(13) # metinsel olarak 13'u donusturdu
+y=int(13.42) # tam sayi olarak donusturdu
+z=float(13) # ondalikli sayi olarak donusturdu
+
+print(x)
+print(y)
+print(z)
+
+v="python 3.14.4"
+
+# 'type' verilen degiskenin hangi veri turunde oldugunu soyleyen bir fonksiyon
+print(type(z))
+print(type(v))
+
+# variable case sensitive/harfe duyarli degiskenler
+
+# her ikisi ayni degil cunku biri buyu biri kucuk harf ile basladi
+name="halil"
+Name='atlas' # tek ' tirnakla yazdirma veya degisken atama islemi yapilabilir
